0acf24c0b2de4c3155f99af3b10c618ec6f2deac
Elijah Tarr <elijahotarr@gmail.com>
2024-03-30 15:27:26 -0400

    fix merge?

---
+++ backend/src/main/java/com/ethan5/dto/Track.java
@@ -0,0 +1,14 @@
+package com.ethan5.dto;
+
+public class Track {
+    private String name; // Matches the "name" field in the JSON
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
