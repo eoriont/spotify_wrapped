a4bb3fb6f837c56a9bf56bfcf9545176c9a522e6
katiewang22 <katie.wang1950f@gmail.com>
2024-04-11 00:06:04 -0400

    fixed checkstyle errors

--- SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/ui/dashboard/DashboardFragment.java
+++ SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/ui/dashboard/DashboardFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spotifywrappedapp.databinding.FragmentDashboardBinding;
 
@@ -17,16 +15,11 @@ public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        //final TextView textView = binding.textDashboard;
-        //dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
