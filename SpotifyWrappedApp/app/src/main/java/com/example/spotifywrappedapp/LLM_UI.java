1e7a9f4245e877ccae7aaeaf88bd6b6d80419290
unknown <salonibedi44@gmail.com>
2024-04-10 23:49:15 -0400

    added screen for LLM output of music personality

---
+++ SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/LLM_UI.java
@@ -0,0 +1,14 @@
+package com.example.spotifywrappedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LLM_UI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llm_ui);
+    }
+}
