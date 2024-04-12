a5eb9ddc21e41b0f00d8a957ae86ae2462411e9d
katiewang22 <katie.wang1950f@gmail.com>
2024-04-12 11:54:06 -0400

    checkstyle fix

--- SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/ui/home/HomeFragment.java
+++ SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/ui/home/HomeFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
