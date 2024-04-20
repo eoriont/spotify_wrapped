82e185a11cf967e07b5ca6d377e0f88baf476996
michaelhyi <michaelyi2004@gmail.com>
2024-04-20 17:15:51 -0400

    feat(api-url): swapped api url with deployed api url

--- SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/apiservices/RetrofitClient.java
+++ SpotifyWrappedApp/app/src/main/java/com/example/spotifywrappedapp/apiservices/RetrofitClient.java
@@ -1,5 +1,15 @@
 package com.example.spotifywrappedapp.apiservices;
 
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -7,7 +17,8 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public final class RetrofitClient {
 
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL
+            = "https://ec2-34-209-168-238.us-west-2.compute.amazonaws.com/";
 
     private RetrofitClient() {
         throw new UnsupportedOperationException();
@@ -19,8 +30,60 @@ public final class RetrofitClient {
                     .baseUrl(BASE_URL)
                     .addConverterFactory(ScalarsConverterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(getUnsafeOkHttpClient())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(
+                                java.security.cert.X509Certificate[] chain,
+                                String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(
+                                java.security.cert.X509Certificate[] chain,
+                                String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[]
+                        getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts,
+                        new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext
+                    .getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(
+                    sslSocketFactory,
+                    (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
